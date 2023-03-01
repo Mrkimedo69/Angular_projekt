5a21955d31be882d120e112fa24aab26e944ae4f
mrkimedo69 <luka.mavric26051999@gmail.com>
2023-03-01 15:10:52 +0100

    fixes

--- Angular_projekt/src/app/app.module.ts
+++ Angular_projekt/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/components/header/header.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { FeaturesModule } from './feature/feature.module';
